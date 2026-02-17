d6bf745964429dfe6f8e0cb7526c07f13eccbf6a
Gaurav Vishwakarma <gauravvishwakarma071@gmail.com>
2026-02-17 13:44:42 +0530

    fetching data in getData function by adding and using axios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import axios from 'axios'
 
 const App = () => {
-  return (
-    <div>App</div>  
-  )
-}
 
-export default App
+  const getData = async () => {
+    try {
+      const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+      console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+      <div className='h-screen bg-black text-white p-4'>
+        <button onClick={getData} className='bg-green-600 active:scale-95 mb-3 rounded px-5 py-2'>
+          getData
+        </button> 
+      </div>
+    )
+  }
+
+  export default App
