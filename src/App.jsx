0b3a37345b96f37a6a11eeaf091e6288e51e6252
Gaurav Vishwakarma <gauravvishwakarma071@gmail.com>
2026-02-18 13:04:55 +0530

    feat: update ( Implemented pagination logic and improve loading state in App component)

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from 'react'
 const App = () => {
 
   const [userData, setUserData] = useState([])
+  let [pageValue, setPageValue] = useState(1)
 
   //Fecthing user data via picsum dummy api
   const getData = async () => {
     try {
-      const res = await axios.get('https://picsum.photos/v2/list?page=2&limit=40')
+      const res = await axios.get(`https://picsum.photos/v2/list?page=${pageValue}&limit=40`)
       setUserData(res.data)
     } catch (err) {
       console.log(err)
@@ -17,10 +18,10 @@ const App = () => {
   //useEffect to trigger function(getData)
   useEffect(() => {
     getData()
-  }, [])
+  }, [pageValue])
 
   //default value
-  let printUserData = <h3 className='text-white font-bold'>User are not found</h3>
+  let printUserData = <h4 className='text-white font-bold absolute top-1/2 left-1/2 -translate-y-1 -translate-x-1'>Loading...</h4>
 
   //diplay user data (logic)
   if (userData.length > 0) {
@@ -44,7 +45,6 @@ const App = () => {
   return (
     //Main screen
     <div className=' bg-gray-900 overflow-auto min-h-screen p-2'>
-
       {/* grid view for user (image data) */}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 p-2'>
         {printUserData}
@@ -52,8 +52,26 @@ const App = () => {
 
       {userData.length > 0 && (
         <div className='flex justify-center gap-6 items-center p-4'>
-          <button className='bg-amber-300 text-sm text-black rounded px-4 py-2 font-semibold active:scale-90'>Prev</button>
-          <button className='bg-amber-300 text-sm text-black rounded px-4 py-2 font-semibold active:scale-90'>Next</button>
+          <button
+            className='bg-amber-300 text-sm text-black rounded px-4 py-2 font-semibold active:scale-90'
+            onClick={() => {
+              if (pageValue > 1) {
+                setUserData([])
+                setPageValue(pageValue - 1)
+              }
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className='bg-amber-300 text-sm text-black rounded px-4 py-2 font-semibold active:scale-90'
+            onClick={() => {
+              setUserData([])
+              setPageValue(pageValue + 1)
+            }}
+          >
+            Next
+          </button>
         </div>
       )}
     </div>
